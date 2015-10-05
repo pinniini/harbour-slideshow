3d9c16a17d43d40da52a2338b59d6dc97a234a07
Åke Engelbrektson <eson57@users.noreply.github.com>
2015-10-05 22:36:15 +0200

    Create harbour-slideshow-sv.ts

---
+++ translations/harbour-slideshow-sv.ts
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Slideshow</source>
+        <translation>Om Slideshow</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>Version </source>
+        <translation>Version </translation>
+    </message>
+    <message>
+        <source>Joni Korhonen, also known as pinniini</source>
+        <translation>Joni Korhonen, också känd som pinniini</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licens</translation>
+    </message>
+    <message>
+        <source>Code</source>
+        <translation>Kod</translation>
+    </message>
+    <message>
+        <source>Ideas/Issues</source>
+        <translation>Idéer/Frågor</translation>
+    </message>
+    <message>
+        <source>By who</source>
+        <translation>Av vem</translation>
+    </message>
+    <message>
+        <source>Slideshow is an application to automatically view your awesome pictures. It works folder-based, i.e. you select a folder which contains pictures and start the slideshow from the pulley menu. Simple!</source>
+        <translation>Slideshow är ett program för att visa dina häftiga bilder. Det fungerar mappbaserat. Det vill säga, du väljer en bildmapp och startar bildspelet från rullgardinsmenyn. Enkelt!</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Slideshow</source>
+        <translation>Slideshow</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Start slideshow in current folder</source>
+        <translation>Starta bildspel i aktuell mapp</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Go up</source>
+        <translation>Upp en nivå</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Slideshow interval</source>
+        <translation>Bildintervall</translation>
+    </message>
+    <message>
+        <source>Loop pictures</source>
+        <translation>Upprepa</translation>
+    </message>
+    <message>
+        <source>Slideshow starts over when reaching the end.</source>
+        <translation>Bildspelet börjar om när sista bilden visas.</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>seconds</source>
+        <translation>sekunder</translation>
+    </message>
+    <message>
+        <source>Stop when minimized</source>
+        <translation>Stoppa vid minimering</translation>
+    </message>
+    <message>
+        <source>Slideshow will be stopped when pushed minimized.</source>
+        <translation>Bildspelet stoppas när appen minimeras.</translation>
+    </message>
+</context>
+</TS>
