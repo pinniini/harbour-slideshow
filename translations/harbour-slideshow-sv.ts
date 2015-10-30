1b2b593aad75235223c17020f73f7bee087e56a2
Åke Engelbrektson <eson57@users.noreply.github.com>
2015-10-30 13:37:56 +0100

    Update harbour-slideshow-sv.ts

--- translations/harbour-slideshow-sv.ts
+++ translations/harbour-slideshow-sv.ts
@@ -4,57 +4,70 @@
 <context>
     <name>AboutPage</name>
     <message>
-        <source>About Slideshow</source>
-        <translation>Om Slideshow</translation>
-    </message>
-    <message>
-        <source>Info</source>
-        <translation>Info</translation>
-    </message>
-    <message>
-        <source>Version </source>
-        <translation>Version </translation>
-    </message>
-    <message>
-        <source>Joni Korhonen, also known as pinniini</source>
-        <translation>Joni Korhonen, också känd som pinniini</translation>
-    </message>
-    <message>
-        <source>License</source>
-        <translation>Licens</translation>
-    </message>
-    <message>
-        <source>Code</source>
-        <translation>Kod</translation>
-    </message>
-    <message>
-        <source>Ideas/Issues</source>
-        <translation>Idéer/Frågor</translation>
-    </message>
-    <message>
-        <source>By who</source>
-        <translation>Av vem</translation>
-    </message>
-    <message>
-        <source>Slideshow is an application to automatically view your awesome pictures. It works folder-based, i.e. you select a folder which contains pictures and start the slideshow from the pulley menu. Simple!</source>
-        <translation>Slideshow är ett program för att visa dina häftiga bilder. Det fungerar mappbaserat. Det vill säga, du väljer en bildmapp och startar bildspelet från rullgardinsmenyn. Enkelt!</translation>
-    </message>
-    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="41"/>
         <source>Found bugs? Got some great improvement ideas? Please report them to github and I am happy to look them through :)</source>
         <translation>Hittat några fel? Har du några bra förbättringsidéer? Rapportera till GitHub så skall jag gärna titta igenom det. :)</translation>
     </message>
     <message>
+        <location filename="../qml/pages/AboutPage.qml" line="65"/>
+        <source>About Slideshow</source>
+        <translation>Om Slideshow</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="69"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="73"/>
+        <source>Slideshow is an application to automatically view your awesome pictures. It works folder-based, i.e. you select a folder which contains pictures and start the slideshow from the pulley menu. Simple!</source>
+        <translation>Slideshow är ett program för att visa dina häftiga bilder. Det fungerar mappbaserat. Det vill säga, du väljer en bildmapp och startar bildspelet från rullgardinsmenyn. Enkelt!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="80"/>
+        <source>Version </source>
+        <translation>Version </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="85"/>
+        <source>By who</source>
+        <translation>Av vem</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="89"/>
+        <source>Joni Korhonen, also known as pinniini</source>
+        <translation>Joni Korhonen, också känd som pinniini</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="96"/>
+        <source>License</source>
+        <translation>Licens</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="110"/>
+        <source>Code</source>
+        <translation>Kod</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="126"/>
         <source>Credits</source>
         <translation>Tack</translation>
     </message>
     <message>
+        <location filename="../qml/pages/AboutPage.qml" line="134"/>
         <source>Swedish translation by: Åke Engelbrektson</source>
         <translation>Svensk översättning av: Åke Engelbrektson</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="138"/>
+        <source>Ideas/Issues</source>
+        <translation>Idéer/Frågor</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
+        <location filename="../qml/cover/CoverPage.qml" line="59"/>
         <source>Slideshow</source>
         <translation>Slideshow</translation>
     </message>
@@ -62,22 +75,32 @@
 <context>
     <name>MainPage</name>
     <message>
-        <source>Start slideshow in current folder</source>
-        <translation>Starta bildspel i aktuell mapp</translation>
-    </message>
-    <message>
-        <source>Settings</source>
-        <translation>Inställningar</translation>
-    </message>
-    <message>
-        <source>Go up</source>
-        <translation>Upp en nivå</translation>
-    </message>
-    <message>
+        <location filename="../qml/pages/MainPage.qml" line="89"/>
+        <location filename="../qml/pages/MainPage.qml" line="272"/>
         <source>About</source>
         <translation>Om</translation>
     </message>
     <message>
+        <location filename="../qml/pages/MainPage.qml" line="95"/>
+        <location filename="../qml/pages/MainPage.qml" line="273"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="101"/>
+        <location filename="../qml/pages/MainPage.qml" line="274"/>
+        <source>Start slideshow in current folder</source>
+        <translation>Starta bildspel i aktuell mapp</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="49"/>
+        <location filename="../qml/pages/MainPage.qml" line="280"/>
+        <source>Go up</source>
+        <translation>Upp en nivå</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="50"/>
+        <location filename="../qml/pages/MainPage.qml" line="277"/>
         <source>Start from here</source>
         <translation>Starta härifrån</translation>
     </message>
@@ -85,48 +108,70 @@
 <context>
     <name>SettingsPage</name>
     <message>
-        <source>Slideshow interval</source>
-        <translation>Bildintervall</translation>
-    </message>
-    <message>
-        <source>Loop pictures</source>
-        <translation>Upprepa</translation>
-    </message>
-    <message>
-        <source>Slideshow starts over when reaching the end.</source>
-        <translation>Bildspelet börjar om när sista bilden visas.</translation>
-    </message>
-    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="78"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="208"/>
         <source>Settings</source>
         <translation>Inställningar</translation>
     </message>
     <message>
-        <source>seconds</source>
-        <translation>sekunder</translation>
-    </message>
-    <message>
-        <source>Stop when minimized</source>
-        <translation>Stoppa vid minimering</translation>
-    </message>
-    <message>
-        <source>Slideshow will be stopped when pushed minimized.</source>
-        <translation>Bildspelet stoppas när appen minimeras.</translation>
-    </message>
-    <message>
-        <source>Language:</source>
-        <translation>Språk:</translation>
-    </message>
-    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="116"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="212"/>
         <source>second</source>
         <translation>sekund</translation>
     </message>
     <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="117"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="213"/>
+        <source>seconds</source>
+        <translation>sekunder</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="125"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="211"/>
+        <source>Slideshow interval</source>
+        <translation>Bildintervall</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="134"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="214"/>
+        <source>Loop pictures</source>
+        <translation>Upprepa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="135"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="215"/>
+        <source>Slideshow starts over when reaching the end.</source>
+        <translation>Bildspelet börjar om när sista bilden visats.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="145"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="216"/>
         <source>Random order</source>
         <translation>Blanda</translation>
     </message>
     <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="146"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="217"/>
         <source>Slideshow plays the pictures in a random order.</source>
         <translation>Slideshow spelar upp bilderna slumpmässigt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="156"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="218"/>
+        <source>Stop when minimized</source>
+        <translation>Stoppa vid minimering</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="157"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="219"/>
+        <source>Slideshow will be stopped when pushed minimized.</source>
+        <translation>Bildspelet stoppas när appen minimeras.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="167"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="220"/>
+        <source>Language:</source>
+        <translation>Språk:</translation>
     </message>
 </context>
 </TS>
