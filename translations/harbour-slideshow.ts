88a4850780e37dc2d48bce390687db9778d59038
Åke Engelbrektson <eson57@users.noreply.github.com>
2022-06-01 13:44:25 +0200

    Add files via upload

--- translations/harbour-slideshow.ts
+++ translations/harbour-slideshow.ts
@@ -1,202 +1,202 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="sv">
 <context>
     <name></name>
     <message id="slideshowlist-no-slideshows">
-        <source></source>
+        <source>No slideshows...</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="menu-add-slideshow">
-        <source></source>
+        <source>Create slideshow</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="menu-add-music">
-        <source></source>
+        <source>Add background music</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="menu-add-files">
-        <source></source>
+        <source>Add pictures from gallery</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="menu-quickstart-slideshow">
-        <source></source>
+        <source>Start quick slideshow</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="menu-start-slideshow">
-        <source></source>
+        <source>Start slideshow</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="slideshow-background-music">
-        <source></source>
+        <source>Background music</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="slideshow-images">
-        <source></source>
+        <source>Pictures</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="slideshow-name-label">
-        <source></source>
+        <source>Name</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="slideshow-name-placeholder">
-        <source></source>
+        <source>Slideshow name</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="slideshowlist-header">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="label-settings">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="label-language">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="menu-settings">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="second">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="seconds">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-slideshow-interval-label">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-header-ui">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-loop">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-loop-description">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-random">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-random-description">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-stop-background">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-stop-background-description">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="quick-folderpicker-title">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="menu-add-files-filesystem">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="menu-add-files-folder">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-select-folder-from-root">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-select-folder-from-root-description">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="about">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="about-info-header">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="about-info">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="version">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="about-by-who-header">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="about-licence-header">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="about-code-header">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="about-contributing-header">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="about-issues">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-loop-background-music">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-loop-background-music-description">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="menu-clear">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="action-clearing-images">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="action-clearing-music">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="image-info-error">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-hires-images">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-hires-images-description">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-header-slideshow">
-        <source></source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="settings-header-imageViewer">
-        <source></source>
+        <source>Slideshows</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="slideshow-menu-remove">
-        <source></source>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="label-settings">
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="label-language">
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="menu-settings">
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="second">
+        <source>second</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="seconds">
+        <source>seconds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-slideshow-interval-label">
+        <source>Interval</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-header-ui">
+        <source>User interface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-loop">
+        <source>Loop pictures</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-loop-description">
+        <source>Slideshow starts over when reaching the end.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-random">
+        <source>Random order</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-random-description">
+        <source>Slideshow plays the pictures in a random order.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-stop-background">
+        <source>Stop when in background</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-stop-background-description">
+        <source>Slideshow will be stopped when application is pushed to background.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="quick-folderpicker-title">
+        <source>Select folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="menu-add-files-filesystem">
+        <source>Add pictures from filesystem</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="menu-add-files-folder">
+        <source>Add folder containing pictures</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-select-folder-from-root">
+        <source>Select folder from root</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-select-folder-from-root-description">
+        <source>Navigation starts from root (&quot;/&quot;) when selecting folder.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="about">
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="about-info-header">
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="about-info">
+        <source>Slideshow is an application to automatically view your awesome pictures. You can quickly select a folder which contains pictures and start the slideshow. Or you can create and save a slideshow containing selected pictures and background music.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="version">
+        <source>Version %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="about-by-who-header">
+        <source>By who</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="about-licence-header">
+        <source>Licence</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="about-code-header">
+        <source>Code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="about-contributing-header">
+        <source>Contributing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="about-issues">
+        <source>Found bugs? Got some great improvement ideas? Please report them to github and I am happy to look them through :)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-loop-background-music">
+        <source>Loop background music</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-loop-background-music-description">
+        <source>Loop background music list.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="menu-clear">
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="action-clearing-images">
+        <source>Clearing pictures</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="action-clearing-music">
+        <source>Clearing music</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="image-info-error">
+        <source>Error occured while loading the picture.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-hires-images">
+        <source>High resolution images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-hires-images-description">
+        <source>Load images in full resolution to image viewer. Unchecking this increases image load performance.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-header-slideshow">
+        <source>Slideshow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="settings-header-imageViewer">
+        <source>Image viewer</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
