07cf52bd91c713b5a469b8eee60bc3b0660f6c53
Åke Engelbrektson <eson57@users.noreply.github.com>
2022-06-01 13:43:11 +0200

    Create harbour-slideshow-sv.ts
    Add Swedish translation.
    I also thing the harbour-slideshow.ts file should have source, and no translations, to be translated.  ;)

---
+++ translations/harbour-slideshow-sv.ts
@@ -0,0 +1,203 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="slideshowlist-no-slideshows">
+        <source>No slideshows...</source>
+        <translation>Inga bildspel...</translation>
+    </message>
+    <message id="menu-add-slideshow">
+        <source>Create slideshow</source>
+        <translation>Skapa bildspel</translation>
+    </message>
+    <message id="menu-add-music">
+        <source>Add background music</source>
+        <translation>Lägg till bakgrundsmusik</translation>
+    </message>
+    <message id="menu-add-files">
+        <source>Add pictures from gallery</source>
+        <translation>Lägg till bilder från Galleri</translation>
+    </message>
+    <message id="menu-quickstart-slideshow">
+        <source>Start quick slideshow</source>
+        <translation>Starta snabbildspel</translation>
+    </message>
+    <message id="menu-start-slideshow">
+        <source>Start slideshow</source>
+        <translation>Starta bildspel</translation>
+    </message>
+    <message id="slideshow-background-music">
+        <source>Background music</source>
+        <translation>Bakgrundsmusik</translation>
+    </message>
+    <message id="slideshow-images">
+        <source>Pictures</source>
+        <translation>Bilder</translation>
+    </message>
+    <message id="slideshow-name-label">
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message id="slideshow-name-placeholder">
+        <source>Slideshow name</source>
+        <translation>Bildspelsnamn</translation>
+    </message>
+    <message id="slideshowlist-header">
+        <source>Slideshows</source>
+        <translation>Bildspel</translation>
+    </message>
+    <message id="slideshow-menu-remove">
+        <source>Remove</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message id="label-settings">
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message id="label-language">
+        <source>Language</source>
+        <translation>Språk</translation>
+    </message>
+    <message id="menu-settings">
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message id="second">
+        <source>second</source>
+        <translation>sekund</translation>
+    </message>
+    <message id="seconds">
+        <source>seconds</source>
+        <translation>sekunder</translation>
+    </message>
+    <message id="settings-slideshow-interval-label">
+        <source>Interval</source>
+        <translation>Intervall</translation>
+    </message>
+    <message id="settings-header-ui">
+        <source>User interface</source>
+        <translation>Användargränssnitt</translation>
+    </message>
+    <message id="settings-loop">
+        <source>Loop pictures</source>
+        <translation>Loopa bilder</translation>
+    </message>
+    <message id="settings-loop-description">
+        <source>Slideshow starts over when reaching the end.</source>
+        <translation>Bildspelet börjar om vid slut.</translation>
+    </message>
+    <message id="settings-random">
+        <source>Random order</source>
+        <translation>Slumpvis ordning</translation>
+    </message>
+    <message id="settings-random-description">
+        <source>Slideshow plays the pictures in a random order.</source>
+        <translation>Bildspelet visar bilderna slumpvis.</translation>
+    </message>
+    <message id="settings-stop-background">
+        <source>Stop when in background</source>
+        <translation>Stoppa om appen är i bakgrunden</translation>
+    </message>
+    <message id="settings-stop-background-description">
+        <source>Slideshow will be stopped when application is pushed to background.</source>
+        <translation>Bildspelet stoppas om appen placeras i bakgrunden.</translation>
+    </message>
+    <message id="quick-folderpicker-title">
+        <source>Select folder</source>
+        <translation>Välj mapp</translation>
+    </message>
+    <message id="menu-add-files-filesystem">
+        <source>Add pictures from filesystem</source>
+        <translation>Lägg till bilder från filsystemet</translation>
+    </message>
+    <message id="menu-add-files-folder">
+        <source>Add folder containing pictures</source>
+        <translation>Lägg till mappar som innehåller bilder</translation>
+    </message>
+    <message id="settings-select-folder-from-root">
+        <source>Select folder from root</source>
+        <translation>Välj mapp från root</translation>
+    </message>
+    <message id="settings-select-folder-from-root-description">
+        <source>Navigation starts from root (&quot;/&quot;) when selecting folder.</source>
+        <translation>Navigering startar från root (&quot;/&quot;) när mapp väljs.</translation>
+    </message>
+    <message id="about">
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message id="about-info-header">
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message id="about-info">
+        <source>Slideshow is an application to automatically view your awesome pictures. You can quickly select a folder which contains pictures and start the slideshow. Or you can create and save a slideshow containing selected pictures and background music.</source>
+        <translation>Slideshow är ett program för att automatiskt visa dina fantastiska bilder. Du kan snabbt välja en mapp som innehåller bilder och starta bildspelet. Eller så kan du skapa och spara ett bildspel som innehåller valda bilder och bakgrundsmusik.</translation>
+    </message>
+    <message id="version">
+        <source>Version %1</source>
+        <translation>Version %1</translation>
+    </message>
+    <message id="about-by-who-header">
+        <source>By who</source>
+        <translation>Av vem</translation>
+    </message>
+    <message id="about-licence-header">
+        <source>Licence</source>
+        <translation>Licens</translation>
+    </message>
+    <message id="about-code-header">
+        <source>Code</source>
+        <translation>Kod</translation>
+    </message>
+    <message id="about-contributing-header">
+        <source>Contributing</source>
+        <translation>Bidrar</translation>
+    </message>
+    <message id="about-issues">
+        <source>Found bugs? Got some great improvement ideas? Please report them to github and I am happy to look them through :)</source>
+        <translation>Hittat fel? Har du några bra förbättringsidéer? Vänligen rapportera dem till GitHub och jag skall gärna titta igenom dem. :)</translation>
+    </message>
+    <message id="settings-loop-background-music">
+        <source>Loop background music</source>
+        <translation>Loopa bakgrundsmusik</translation>
+    </message>
+    <message id="settings-loop-background-music-description">
+        <source>Loop background music list.</source>
+        <translation>Loopa bakgrundsmusiklistan.</translation>
+    </message>
+    <message id="menu-clear">
+        <source>Clear</source>
+        <translation>Rensa</translation>
+    </message>
+    <message id="action-clearing-images">
+        <source>Clearing pictures</source>
+        <translation>Tar bort bilder</translation>
+    </message>
+    <message id="action-clearing-music">
+        <source>Clearing music</source>
+        <translation>Tar bort musik</translation>
+    </message>
+    <message id="image-info-error">
+        <source>Error occured while loading the picture.</source>
+        <translation>Ett fel uppstod vid inläsning av bilden.</translation>
+    </message>
+    <message id="settings-hires-images">
+        <source>High resolution images</source>
+        <translation>Högupplösta bilder</translation>
+    </message>
+    <message id="settings-hires-images-description">
+        <source>Load images in full resolution to image viewer. Unchecking this increases image load performance.</source>
+        <translation>Läs in bilder i full upplösning till bildläsaren. Om du avmarkerar detta ökar visningsprestandan.</translation>
+    </message>
+    <message id="settings-header-slideshow">
+        <source>Slideshow</source>
+        <translation>Bildspel</translation>
+    </message>
+    <message id="settings-header-imageViewer">
+        <source>Image viewer</source>
+        <translation>Bildläsare</translation>
+    </message>
+</context>
+</TS>
